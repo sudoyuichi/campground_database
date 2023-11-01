33725601e88bdcdc3d11ffe832fb1b10f066265e
tsuyoshi <tsuyoshi3627@icloud.com>
2023-11-01 21:33:37 +0900

    var2 testテーブルの削除取消、ユニーク制約追加、ステータスをint型変更

--- database.sql
+++ database.sql
@@ -1,14 +1,21 @@
 
 CREATE DATABASE TestDB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
+CREATE TABLE TestTable (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     password_reset_token VARCHAR(100),
     password_reset_expiration DATETIME,
-    registration_status TINYINT(1) DEFAULT 0,
+    registration_status INT DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
     deleted_at DATETIME
