075b8165bb6685fd63a6516a697f81187931c550
sudoyuichi <sudo.yuichi@gmail.com>
2023-12-31 20:10:56 +0900

    設計の都合上必要だったので、作成した。

---
+++ System/Control/mainControl.php
@@ -0,0 +1,55 @@
+<?php
+
+$rootPath = __DIR__ . '/..';
+
+require_once $rootPath . '/define.php';
+require_once $rootPath . CONTROL_PATH . '/commonControl.php';  // commonControl.phpをインクルード
+require_once $rootPath . MODEL_PATH . '/userModel.php';
+require_once $rootPath . VIEW_PATH . '/vendor/smarty/smarty/libs/Smarty.class.php';
+
+class mainControl extends Smarty {
+
+    private $rootPath;
+    private $common;  // commonControlのインスタンスを保持するプロパティ
+
+    public function __construct() {
+        global $rootPath;
+        $this->rootPath = $rootPath;
+        // commonControlのインスタンスを作成
+        $this->common = new commonControl();
+        parent::__construct();
+        $this->setTemplateDir($this->rootPath . VIEW_PATH . '/templates/');
+        $this->setCompileDir($this->rootPath . VIEW_PATH . '/templates_c/');
+        $this->setCacheDir($this->rootPath . VIEW_PATH . '/cache/');
+        $this->setConfigDir($this->rootPath . VIEW_PATH . '/configs/');
+        session_start();
+    }
+
+    /**
+     * 指定されたモードに基づいて操作を実行し、テンプレートを表示するメソッド
+     * mode = registerならユーザ登録処理を実行
+     * @param string $mode モード（'index' または 'register'）
+     */
+    public function execute($mode) {
+        $templateDir = 'Main/';
+        $errorMsg = null;
+        $this->ensureSessionStarted();
+        switch ($mode) {
+           
+            default:
+                $_SESSION['name'] = 'test';
+                $templateDir .= 'main.tpl';
+                break;
+            }
+        $this->assign([
+            'errorMsg' => $errorMsg,
+        ]);
+        $this->display($templateDir);
+    }
+
+    private function ensureSessionStarted() {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+}
